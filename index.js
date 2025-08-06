c7c6196df4aae20465159d9428a3ebbd2fbce08c
mission122210 <gz707985@gmail.com>
2025-08-06 14:25:34 -0400

    update

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ require("dotenv").config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors())
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["Content-Type"],
+    })
+)
+
 app.use(express.json())
 
 // File storage setup
